2bbe485584e186ded2c81a54e426c825f95461bb
babykachima <nguyenhoangtuan.workvn@gmail.com>
2023-03-05 01:15:54 +0700

    init project and implement overview page

---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,62 @@
+import 'package:english_app/pages/home_page.dart';
+import 'package:english_app/values/app_assets.dart';
+import 'package:english_app/values/app_colors.dart';
+import 'package:english_app/values/app_styles.dart';
+import 'package:flutter/material.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.primaryColor,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(children: [
+          Expanded(
+              child: Container(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              'Welcome to',
+              style: AppStyles.h3,
+            ),
+          )),
+          Expanded(
+              child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                'English',
+                style: AppStyles.h2.copyWith(
+                    fontWeight: FontWeight.w700, color: AppColors.blackGrey),
+              ),
+              Text(
+                'Qoutes"',
+                textAlign: TextAlign.right,
+                style: AppStyles.h4
+                    .copyWith(fontWeight: FontWeight.w400, height: 0.5),
+              )
+            ],
+          )),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 64),
+              child: RawMaterialButton(
+                fillColor: AppColors.lighBlue,
+                shape: const CircleBorder(),
+                onPressed: () {
+                  Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(builder: (_) => const HomePage()),
+                      (route) => false);
+                },
+                child: Image.asset(AppAssets.rightArrow),
+              ),
+            ),
+          )
+        ]),
+      ),
+    );
+  }
+}
---
+++ lib/values/app_styles.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class FontFamily {
+  static const sen = 'Sen';
+}
+
+class AppStyles {
+  static TextStyle h1 = const TextStyle(
+      fontSize: 109.66, color: Colors.white, fontFamily: FontFamily.sen);
+  static TextStyle h2 = const TextStyle(
+      fontSize: 67.77, color: Colors.white, fontFamily: FontFamily.sen);
+  static TextStyle h3 = const TextStyle(
+      fontSize: 41.89, color: Colors.white, fontFamily: FontFamily.sen);
+  static TextStyle h4 = const TextStyle(
+      fontSize: 25.89, color: Colors.white, fontFamily: FontFamily.sen);
+  static TextStyle h5 = const TextStyle(
+      fontSize: 16, color: Colors.white, fontFamily: FontFamily.sen);
+  static TextStyle h6 = const TextStyle(
+      fontSize: 9.89, color: Colors.white, fontFamily: FontFamily.sen);
+}
